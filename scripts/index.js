ceb521c8591dbb49e544e6fa52039f526dd87e3a
Jihan Nugraha <68485092+jihan431@users.noreply.github.com>
2025-08-27 22:52:52 +0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -18,7 +18,7 @@ function validateForm() {
       showConfirmButton: false,
       timer: 1500,
     }).then(function () {
-      window.location.href = "birthday.html";
+      window.location.href = "my/birthday.html";
     });
   } else {
     Swal.fire({
